444cd4a1f622686ff29e1f0ac5b2a1251f61123b
Devaraj <dsbasevadeyar57@gmail.com>
2026-04-21 01:57:28 +0530

    Functions in javaScript

---
+++ functions.js
@@ -0,0 +1,10 @@
+//function is a block of code that performs a specific task, can be invoked whenever needed
+
+function myFunction(){
+    console.log("welcome to javaScript");
+}
+myFunction();
+
+// for(let i=0;i<5;i++){
+//     myFunction();
+// }
